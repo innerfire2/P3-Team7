0e8622585f74d284f395ec424e93c0b75b8755c8
innerfire2 <73815918+innerfire2@users.noreply.github.com>
2022-01-24 14:27:47 +0100

    personen und ar test

--- 2021-11-25-personen-db/classes/Person.js
+++ 2021-11-25-personen-db/classes/Person.js
@@ -1,5 +1,7 @@
 import { persons as personsData } from "../data/model_data.js"; // Nur in der Person Klasse wird die "Datenbank" geladen. Das "as" gibt uns einen besseren Namen um es nicht mit der Klasse zu verwechseln.
 import { v4 as uuid } from "uuid";
+import database from "../database.js";
+
 export default class Person {
   constructor({ id, name, lastName }) {
     // Hier werden aus dem übergebenen Parameter id und name herausgeladen; Der Parameter ist eigentlich so etwas: { id: 1, name: "Peter" }
@@ -14,13 +16,16 @@ export default class Person {
 
   // Liefert zu allen Datensätzen eine Person zurück
   static all() {
-    return personsData.map((object) => {
-      // personsData ist eigentlich nur ein simples Array. Da wir haben ein Array mit `Person`en haben wollen, wandeln wir mit `map` das Array so für uns um.
-      return new Person(object); // Baut aus dem Element im Array eine neue Person
+    // return personsData.map((object) => {
+    //   // personsData ist eigentlich nur ein simples Array. Da wir haben ein Array mit `Person`en haben wollen, wandeln wir mit `map` das Array so für uns um.
+    //   return new Person(object); // Baut aus dem Element im Array eine neue Person
+    // });
+
+    db.all("SELECT * FROM persons", function (err, rows) {
+      console.log(rows);
     });
   }
 
-  // Sucht in den Datensätzen nach einen Treffer mit der übergebenen ID
   static find(id) {
     const data = personsData.find((object) => object.id === id); // Durchläfut das Array und gibt den ersten Treffer zurück. Dieser wird in die "data" variabel geschrieben
     if (data) {
@@ -53,11 +58,18 @@ export default class Person {
 
   // Speichert die Person in die "Datenbank". Wir holen uns aus der Person die benötigten Daten. Gespeichert wird nicht die Information über die Klasse, sondern nur die reinen Daten.
   save() {
-    personsData.push({
-      // fügt dem Datensatz-Array ein Eintrag hinzu
-      id: this.id,
-      name: this.name,
-      lastName: this.lastName,
-    });
+    // personsData.push({
+    //   // fügt dem Datensatz-Array ein Eintrag hinzu
+    //   id: this.id,
+    //   name: this.name,
+    //   lastName: this.lastName,
+    // });
+
+    const sql = db.prepare(
+      "INSERT INTO persons (id, first_name, last_name) VALUES (?,?,?)",
+      [this.id, this.firstName, this.lastName]
+    );
+    sql.run([this.id, this.firstName, this.lastName]);
+    sql.finalize();
   }
 }
